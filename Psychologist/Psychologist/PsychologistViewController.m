ae0921b7ed6f2b9e14bf3e3e55f5989304063326
kvnn <kriggen@gmail.com>
2012-11-07 12:10:22 -0800

    Added new project Psychologist

---
+++ Psychologist/Psychologist/PsychologistViewController.m
@@ -0,0 +1,38 @@
+//
+//  PsychologistViewController.m
+//  Psychologist
+//
+//  Created by icrossing on 11/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PsychologistViewController.h"
+
+@interface PsychologistViewController ()
+
+@end
+
+@implementation PsychologistViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    } else {
+        return YES;
+    }
+}
+
+@end
